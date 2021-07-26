0b4d7982610b9e0792ffd049911f7c4aa67bd15b
enzonjagi <amonsdungo@gmail.com>
2021-07-26 13:21:11 +0300

    added three conversion specifiers

--- holberton.h
+++ holberton.h
@@ -9,11 +9,16 @@
  * @id: identifier.
  * @f: corresponding function.
  */
-typedef struct id_func{
-        char *id;
-        void (*f)(char *, va_list);
+typedef struct id_func
+{
+	char *id;
+	void (*f)(char *, va_list);
 } id_f;
 
 void _putchar(char c);
 int _printf(const char *format, ...);
+void p_char(va_list ap);
+void p_str(va_list ap);
+void p_perc(va_list ap);
+
 #endif
