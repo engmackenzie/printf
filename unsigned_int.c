2e471073b9e5777b4a5c21c40c12089a9e34e51f
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-27 15:08:38 +0300

    added lowercase hex notation

--- unsigned_int.c
+++ unsigned_int.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * int p_binary - prints an int int binary
+ * int p_binary - prints an int int binary (handles b)
  * @ap: the int to be printed
  *
  * Return: number of chars printed.
@@ -35,7 +35,7 @@ int p_binary(va_list ap)
 }
 
 /**
- * int p_udec - prints an unsigned int in dec
+ * p_udec - prints an unsigned int in dec (handles u)
  * @ap: uint to be printed.
  *
  * Return: number of chars printed.
@@ -68,4 +68,74 @@ int p_udec(va_list ap)
 	return (len);
 }
 		
+/**
+ * p_oct - prints an unsigned int in octal (handles o)
+ * @ap: uint to be printed.
+ *
+ * Return: number of chars printed.
+ */
+
+int p_oct(va_list ap)
+{
+	unsigned int n, arr[11], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+	
+	if (!n)
+		i++;	
+	while (n > 0)
+	{
+		arr[i] = n % 8;
+		n /= 8;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		_putchar(arr[i] + '0');
+		i--;
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * int p_xhex - prints a uint in lowercase hex
+ * @ap: the int to be printed
+ *
+ * Return: number of chars printed.
+ */
+
+int p_xhex(va_list ap)
+{
+	unsigned int n, arr[33], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+	
+	if (!n)
+		i++;	
+	while (n > 0)
+	{
+		arr[i] = n % 16;
+		n /= 16;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		if (arr[i] < 10)
+			_putchar(arr[i] + '0');
+		else
+			_putchar(arr[i] + '0' + 'a' - ':');
+		i--;
+		len++;
+	}
+
+	return (len);
+}
 
