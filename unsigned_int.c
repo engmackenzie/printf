4ddd5bff7415a131853216ab42a5c7bbc6ef8e49
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-27 14:01:01 +0300

    binary

---
+++ unsigned_int.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * int p_binary - prints an int int binary
+ * @ap: the int to be printed
+ *
+ * Return: number of chars printed.
+ */
+
+int p_binary(va_list ap)
+{
+	unsigned int n, arr[33], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+
+	while (n > 0)
+	{
+		arr[i] = n % 2;
+		n /= 2;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		_putchar(arr[i] + '0');
+		i--;
+		len++;
+	}
+
+	return (len);
+}
+
